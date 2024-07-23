6c604ca99f5e687ea4b75a0a6aec271ad925c9e3
gab <155622983+GabGaitanidis@users.noreply.github.com>
2024-07-23 13:21:59 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+http
+  .createServer(function (req, res) {
+    const q = url.parse(req.url, true);
+    let filename = "";
+    if (q.pathname == "/") {
+      filename = "./index.html";
+    } else if (q.pathname == "/about") {
+      filename = "./about.html";
+    } else if (q.pathname == "/contact-me") {
+      filename = "./contact.html";
+    } else {
+      filename = "./404.html";
+    }
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
